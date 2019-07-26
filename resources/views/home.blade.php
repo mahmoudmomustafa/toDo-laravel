ba63b8c86b76928d5ace49ea4c1917adcf951b1e
Mahmoud Mustafa <mid7od@gmail.com>
2019-07-26 14:26:56 +0200

    style sidebar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,14 @@
-@extends('layouts.app') 
+@extends('layouts.app')
 @section('content')
-<div class="container">
+<div class="container-fluid">
     <div class="content">
         {{-- show tasks --}}
         <div class="row justify-content-center show-task">
-            <div class="col-md-12">
+            <div class="col-lg-8">
                 <div class="card">
-                    <div class="card-header">Your Tasks</div>
+                    <div class="card-header">
+                        <h4 class="font-weight-bold" style="color: #5E52F6">{{ __('Tasks asdasda ada sasda ') }}</h4>
+                    </div>
 
                     <div class="card-body">
                         <div class="tasks">
@@ -20,16 +22,35 @@
                             {{-- @if(isset($tasks)) --}}
                             @foreach ($tasks as $task)
                             <div class="addTask">
-                            <a href="/tasks/{{$task->id}}">
-                                        <div class="add">
-                                           <p>
-                                               {{$task->task_title}}
-                                            </p>
-                                        </div>
-                                    </a>
-                                </div>
+                                <a href="/tasks/{{$task->id}}">
+                                    <div class="add">
+                                        <p>
+                                            {{$task->task_title}}
+                                        </p>
+                                    </div>
+                                </a>
+                            </div>
                             @endforeach
                             {{-- @endif --}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card">
+                    <h4 class="p-4 font-weight-bold" style="color: #5E52F6">{{ __('Tasks') }}<span
+                            class="text-muted pl-1"><small>({{$tasks->count()}})</small></span></h4>
+                    <div class="card-body pt-1">
+                        <div class="tasks">
+                            @foreach ($tasks as $task)
+                            <div class="addTask">
+                                <a class="font-weight-bold" href="/tasks/{{$task->id}}">
+                                    <p>
+                                        {{$task->task_title}}
+                                    </p>
+                                </a>
+                            </div>
+                            @endforeach
                         </div>
                     </div>
                 </div>
