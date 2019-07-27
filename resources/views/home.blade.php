4f8477d1d209669cc0fec60de469b30503536140
Mahmoud Mustafa <mid7od@gmail.com>
2019-07-27 14:19:55 +0200

    minor changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 <div class="col-lg-6 mx-4">
     <div class=" mt-2">
-        <h2 class="show p-4 font-weight-bold" style="color: #5b6f82">{{ __('Welcome Back ') }}
+        <h2 class="shows p-4 font-weight-bold" style="color: #5b6f82">{{ __('Welcome Back ') }}
             <span>'{{ Auth::user()->name}}'</span></h2>
         <div class="card-body home">
             <h3 class="p-4 font-weight-bold float-left" style="color: #5E52F6">Make Your Lists Now</h3>
